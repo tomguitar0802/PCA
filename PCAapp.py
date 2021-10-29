d901b49483229bd447a86462691f4adaf5b99903
tomguitar0802 <78745783+tomguitar0802@users.noreply.github.com>
2021-10-30 03:59:06 +0900

    Update PCAapp.py

--- PCAapp.py
+++ PCAapp.py
@@ -5,7 +5,7 @@ from mpl_toolkits.mplot3d.axes3d import Axes3D
 import seaborn as sns
 from sklearn.decomposition import TruncatedSVD
 import streamlit as st
-sns.set(font="IPAexGothic"))
+sns.set(font="IPAexGothic")
 st.write(plt.rcParams["font.family"])
 Path=st.sidebar.file_uploader('Excel')
 if Path is not None:

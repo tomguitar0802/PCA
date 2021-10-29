6de9c6bbbf066cfe8512853fff7c1b04ed739fee
tomguitar0802 <78745783+tomguitar0802@users.noreply.github.com>
2021-10-30 03:54:40 +0900

    Update PCAapp.py

--- PCAapp.py
+++ PCAapp.py
@@ -5,7 +5,7 @@ from mpl_toolkits.mplot3d.axes3d import Axes3D
 import seaborn as sns
 from sklearn.decomposition import TruncatedSVD
 import streamlit as st
-sns.set(font="IPAexGothic")
+
 st.write(plt.rcParams["font.family"])
 plt.rcParams["font.family"]="IPAexGothic"
 st.write(plt.rcParams["font.family"])
@@ -44,6 +44,7 @@ if Path is not None:
         plt.xlabel("第一主成分の重み")
         plt.ylabel("第二主成分の重み")
         st.pyplot(fig)
+        st.write(plt.rcParams["font.family"])
     if MODE=="3D":
         select1=st.sidebar.selectbox("x軸",np.arange(1,N+1),0)
         select2=st.sidebar.selectbox("y軸",np.arange(1,N+1),1)

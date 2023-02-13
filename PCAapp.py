e1d8fa4658fac3f56099204a5b6d04e13721f4e4
tomguitar0802 <78745783+tomguitar0802@users.noreply.github.com>
2023-02-13 22:01:17 +0900

    Update PCAapp.py

--- PCAapp.py
+++ PCAapp.py
@@ -15,12 +15,12 @@ else:
     
 Processing=st.sidebar.radio("Processing",["Origin","Standardize(µ=0,σ=1)","Normalize(max=1,min=0)"])
 N=st.sidebar.slider("主要素数",3,len(df.columns)-1,3,1)
-select1=st.sidebar.selectbox("x軸",np.arange(1,N+1),0)
-select2=st.sidebar.selectbox("y軸",np.arange(1,N+1),1)
-x_label1=st.sidebar.text_input('xラベル',"comp"+str(select1))
-y_label1=st.sidebar.text_input('yラベル',"comp"+str(select2))
-x_label2=st.sidebar.text_input('xラベル',"w"+str(select1))
-y_label2=st.sidebar.text_input('yラベル',"w"+str(select2))
+select1=st.sidebar.selectbox("x axis",np.arange(1,N+1),0)
+select2=st.sidebar.selectbox("y axis",np.arange(1,N+1),1)
+x_label1=st.sidebar.text_input('x label',"comp"+str(select1))
+y_label1=st.sidebar.text_input('y label',"comp"+str(select2))
+x_label2=st.sidebar.text_input('x label',"w"+str(select1))
+y_label2=st.sidebar.text_input('y label',"w"+str(select2))
 
 st.write("Raw Data")
 st.write(df)
@@ -46,7 +46,7 @@ vecs_list=model_svd.fit_transform(df)
 st.dataframe(pd.DataFrame(vecs_list,columns=c_list,index=df.index))
 st.dataframe(pd.DataFrame(model_svd.components_,columns=df.columns,index=i_list))
 
-
+st.write(f"comp{select1} vs comp{select2}")
 fig,ax=plt.subplots()
 X=vecs_list[:,select1-1]
 Y=vecs_list[:,select2-1]
@@ -56,7 +56,8 @@ for i,(annot_x,annot_y) in enumerate(zip(X,Y)):
 plt.xlabel(x_label1)
 plt.ylabel(y_label1)
 st.pyplot(fig)
-    
+
+st.write(f"w{select1} vs w{select2}")
 fig,ax=plt.subplots()
 X_comp=model_svd.components_[select1-1]
 Y_comp=model_svd.components_[select2-1]
